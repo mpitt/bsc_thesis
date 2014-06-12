3769af415e9c1cf170d80cf3e4c91b6dc87bae9a
Michele Pittoni <michele@pittoni.org>
2014-06-12 16:58:43 +0200

    mp: add different modes

--- message_propagation/message_propagation.py
+++ message_propagation/message_propagation.py
@@ -3,24 +3,30 @@ import numpy as np
 from collections import deque
 from multiprocessing import Pool, cpu_count
 import argparse, re, random, time, os
+import mpr
 
 class MessagePropagation:
-    def __init__(self, graph, jobs, relays=[]):
+    def __init__(self, graph, mode, jobs):
         self.graph = graph
         self.jobs = jobs
-        if relays == []:
-            self.relays = graph.nodes()
-        else:
-            self.relays = relays
+        self.mode = mode
 
     def run(self, times=1000):
         T = [0 for u in self.graph]
         R = [0 for u in self.graph]
         routes_from = [set([(u,v) for v in self.graph if v != u]) for u in self.graph]
         routes_to = [set([(v,u) for v in self.graph if v != u]) for u in self.graph]
+        if self.mode == "mpr":
+            self.mprSolution = mpr.solveMPRProblem(self.graph)
+            originatorSet = set()
+            for s in self.mprSolution.values():
+                originatorSet.update(list(s)[0])
+            originators = list(originatorSet)
+        else:
+            originators = self.graph.nodes()
         for i in range(times):
             known_routes = set()
-            for u in self.graph:
+            for u in originators:
                 rk = self.propagate(u)
                 known_routes = known_routes.union(rk)
             for u in self.graph:
@@ -35,7 +41,10 @@ class MessagePropagation:
     def propagate(self, u):
         message = {}
         message['sender'] = u
-        message['content'] = [u] + self.graph.neighbors(u)
+        if self.mode == "batman":
+            message['content'] = [u]
+        else:
+            message['content'] = [u] + self.graph.neighbors(u)
         q = deque()
         route_knowledge = set()
         received_message = set([u])
@@ -45,7 +54,12 @@ class MessagePropagation:
             u,v = q.popleft()
             n = random.random()
             if (n >= self.graph[u][v]['weight'] and
-                    v not in received_message):
+                    v not in received_message
+                    and (
+                        self.mode != "mpr" or
+                        v in list(self.mprSolution[u])[0]
+                        )
+                    ):
                 received_message.add(v)
                 for i in message['content']:
                     route_knowledge.add((i,v))
@@ -78,10 +92,14 @@ if __name__ == "__main__":
             type=int,
             help="number of workers in the multiprocessing pool",
             default=cpu_count())
+    parser.add_argument("-m", "--mode",
+            choices=["default", "batman", "mpr"],
+            help="propagation mode, see description",
+            default="default")
     args = parser.parse_args()
 
     G = nx.read_gpickle(args.testcase+"/graph.pickle")
-    mp = MessagePropagation(G, args.jobs)
+    mp = MessagePropagation(G, args.mode, args.jobs)
     #testname = re.search(r"(?P<file>[^/]*)$", args.testcase).group("name")
     res = mp.run()
     date = time.strftime("%Y%m%d-%H%M")
