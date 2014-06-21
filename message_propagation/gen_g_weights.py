edc8c4c9f62f5e652541cdde0ff8305a07b79120
Michele Pittoni <michele@pittoni.org>
2014-06-21 18:46:49 +0200

    mp-generator: print more info and clean up the code

--- message_propagation/gen_g_weights.py
+++ message_propagation/gen_g_weights.py
@@ -1,25 +1,31 @@
 import networkx as nx
+import numpy as np
 from scipy.stats import uniform
-import sys, os, argparse
+import sys
+import os
+import argparse
 
 if __name__ == '__main__':
     generators = {
-            'random': nx.fast_gnp_random_graph,
-            'path': nx.path_graph,
-            'complete': nx.complete_graph
+        'random': nx.fast_gnp_random_graph,
+        'path': nx.path_graph,
+        'complete': nx.complete_graph
     }
     parser = argparse.ArgumentParser()
     parser.add_argument("generator", choices=generators.keys())
     parser.add_argument("-n", "--nodes", type=int, required=True)
     parser.add_argument("-p", "--gnpp", type=float)
-    parser.add_argument("-w", "--maxweight", type=float, default=0.2,
-            help="Maximum probability of loss, default 0.2")
+    parser.add_argument(
+        "-w",
+        "--maxweight", type=float,
+        default=0.2,
+        help="Maximum probability of loss, default 0.2")
     parser.add_argument("name")
     args = parser.parse_args()
 
     gen = args.generator
     if gen == 'random':
-        if args.gnpp == None:
+        if args.gnpp is None:
             print "Error: -p is required with the random generator"
             sys.exit(2)
         g = generators[gen](args.nodes, args.gnpp)
@@ -28,19 +34,23 @@ if __name__ == '__main__':
     else:
         g = generators[gen](args.nodes)
 
-    dist = uniform(0,args.maxweight)
-    for e in g.edges_iter():
-        g[e[0]][e[1]]['weight'] = dist.rvs()
-    os.mkdir("cases/"+args.name)
-    nx.write_gpickle(g, "cases/"+args.name+"/graph.pickle")
-    f = open("cases/"+args.name+"/info", "w")
+    dist = uniform(0, args.maxweight)
+    for u, v in g.edges_iter():
+        g[u][v]['weight'] = dist.rvs()
+    os.mkdir("cases/" + args.name)
+    nx.write_gpickle(g, "cases/" + args.name + "/graph.pickle")
+    f = open("cases/" + args.name + "/info", "w")
     f.write("Number of nodes: ")
-    f.write(str(len(g))+"\n")
+    f.write(str(len(g)) + "\n")
     f.write("Number of edges: ")
-    f.write(str(g.number_of_edges())+"\n")
-    f.write("Density: "+str(nx.density(g))+"\n")
+    f.write(str(g.number_of_edges()) + "\n")
+    f.write("Average degree: %8.4f\n" % np.average(g.degree().values()))
+    weights = [float(attr['weight']) for u, v, attr in g.edges_iter(data=True)]
+    f.write("Weights (min/avg/max/std): %f/%f/%f/%f\n" % (
+            min(weights), np.average(weights), max(weights),
+            np.std(weights, dtype=np.float64)))
     f.close()
-    f = open("cases/"+args.name+"/weights", "w")
-    for e in g.edges_iter(data=True):
-        f.write(str(e[2]['weight'])+"\n")
+    f = open("cases/" + args.name + "/weights", "w")
+    for w in weights:
+        f.write("%f\n" % w)
     f.close()
